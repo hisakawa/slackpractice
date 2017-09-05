05e48be6ea9fb66fb853909c63338336d56b5363
ryusei hisakawa <ryuseihisakawa@ryusei-no-MacBook-Air.local>
2017-09-05 15:38:59 +0900

    webhook

---
+++ webhook.py
@@ -0,0 +1,15 @@
+#coding: UTF-8
+
+import slackweb
+
+#IncomingwebhooksのwebhookURL
+slack = slackweb.Slack(url="https://hooks.slack.com/services/*******/*******")
+attachments=[]
+attachment={"pretext": "Incomingwebhooksを使用",
+"color": "good",
+"title": "高知工科大学",
+"title_link": "https://www.kochi-tech.ac.jp",
+"text": "高知工科大学ホームページのリンク",
+}
+attachments.append(attachment)
+slack.notify(text="pythonでの投稿", username="hooksan", attachments=attachments)
